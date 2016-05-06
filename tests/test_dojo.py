9cb7b2d886bfca656b1f4c6dd59fc917feeac8ab
adowell <adowell@stone.com.br>
2016-05-06 11:16:45 -0300

    Renomeia TestCase do dojo e implementa alguns testes basicos

---
+++ tests/test_dojo.py
@@ -0,0 +1,24 @@
+# encoding:utf-8
+import unittest
+from project.run import (generate_sample, rabbit_training)
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.sample = generate_sample()
+
+    def test_single_day(self):
+        self.sample = generate_sample(num_days=1)
+        self.assertEqual(rabbit_training(self.sample), 0)
+
+    def test_no_training(self):
+        kwargs = {
+            'num_days': 0
+        }
+        self.sample = generate_sample(**kwargs)
+        self.assertEqual(rabbit_training(self.sample), 0)
+
+    def test_two_rabbits(self):
+        self.sample = generate_sample(num_days=2)
+        fine = self.sample[1][1]
+        self.assertEqual(rabbit_training(self.sample), fine)
