e178eb95348c2e91ba55cd34098dfd3e628eca3d
adowell <adowell@stone.com.br>
2016-05-10 14:23:20 -0300

    Ajusta criacao aleatoria de samples e implementa solucao encontrada no dojo

--- project/run.py
+++ project/run.py
@@ -1,19 +1,52 @@
+# encoding: utf-8
 import random
 
-DAYS = range(1, 8)
+DAYS = range(1, 7)
 FINE = DAYS + [1000]
 
 
-def generate_sample(num_days=7):
-    input_num = range(random.randint(1, num_days + 1))
-    input_data = map(lambda x: (DAYS[random.randint(0, len(DAYS) - 1)],
-                                FINE[random.randint(0, len(FINE) - 1)]),
+def generate_sample(num_days=-1):
+    if num_days == -1:
+        num_days = random.choice(DAYS)
+    input_num = range(1, num_days + 1)
+    input_data = map(lambda x: (random.choice(DAYS), random.choice(FINE)),
                      input_num)
     return input_data
 
 
 def rabbit_training(sample):
-    return 0
+    current = (0, 0)
+    stack = []
+    fine = 0
+    for rabbit in sample:
+        stack.append([rabbit[0], rabbit[1]])
+        if current[0] == 0:
+            current = decide(stack)
+            stack.remove(current)
+        current[0] -= 1
+        fine += calculate(stack)
+    while(stack):
+        if current[0] == 0:
+            current = decide(stack)
+            stack.remove(current)
+        current[0] -= 1
+        fine += calculate(stack)
+    return fine
+
+
+def decide(stack):
+    best = stack[0]
+    for rabbit in stack:
+        if (float(rabbit[1]) / rabbit[0]) > (float(best[1]) / best[0]):
+            best = rabbit
+    return best
+
+
+def calculate(stack):
+    fine = 0
+    for rabbit in stack:
+        fine += rabbit[1]
+    return fine
 
 if __name__ == '__main__':
     sample = generate_sample()
